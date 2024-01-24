3dcfe5752c6a8bbd5ade1e9ef191bc5d7e775fb4
PremitiveCoding <105321861+iliassazoum@users.noreply.github.com>
2024-01-24 10:49:53 +0100

    k8s

---
+++ scripts.sh
@@ -0,0 +1,12 @@
+kubectl exec -it postgres-7c45964c9b-9wr2w -n twitter -- psql -U postgres
+
+CREATE DATABASE chat;
+CREATE DATABASE email;
+CREATE DATABASE image;
+CREATE DATABASE lists;
+CREATE DATABASE notification;
+CREATE DATABASE tag;
+CREATE DATABASE topic;
+CREATE DATABASE tweet;
+CREATE DATABASE user;
+
